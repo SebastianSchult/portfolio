00b3f0257ce458a30bed5815c72e8d57b4e6993c
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-09-09 15:46:00 +0200

    design nav-bar

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  constructor() {}
+
 }
+
+
+
+
+
