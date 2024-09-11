b5633a12426b5843682aca9b1099b7001c1b812d
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-09-11 20:36:50 +0200

    building skills

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  isVisible = true;
 
+  skills = [
+    { src: 'assets/img/skills/html.png' , description: 'HTML' },
+    { src: 'assets/img/skills/css.png' , description: 'CSS' },
+    { src: 'assets/img/skills/javascript.png' , description: 'Javascript' },
+    { src: 'assets/img/skills/typescript.png' , description: 'Typescript' },
+    { src: 'assets/img/skills/angular.png' , description: 'Angular' },
+    { src: 'assets/img/skills/firebase.png' , description: 'Firebase' },
+    { src: 'assets/img/skills/git.png' , description: 'Git' },
+    { src: 'assets/img/skills/api.svg' , description: 'Rest-API' },
+    { src: 'assets/img/skills/scrum.png' , description: 'Scrum' },
+    { src: 'assets/img/skills/materialDesign.png' , description: 'Material Design' },
+
+    
+    
+
+  ]
 }
