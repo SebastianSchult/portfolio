700aa49b38c3c8ccc80a0e6aaa483b103b3ce5f3
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-10-29 10:49:48 +0100

    building skills and create component portfolio

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent],
+  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
