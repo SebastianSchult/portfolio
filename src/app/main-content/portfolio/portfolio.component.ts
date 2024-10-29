f8d3eeed6cf4eb819b86d9b3d9220292def79a41
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-10-29 19:23:12 +0100

    editing portfolio html ts css

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+
+interface Project {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
 @Component({
   selector: 'app-portfolio',
-  standalone: true,
-  imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
-
+  projects: Project[] = [
+    { title: 'El Pollo Loco', description: 'Ein Spiel über ein verrücktes Huhn', imageUrl: 'assets/el-pollo-loco.png' },
+    { title: 'Task Manager', description: 'Verwalten Sie Ihre Aufgaben', imageUrl: 'assets/task-manager.png' },
+    { title: 'Chat App', description: 'Kommunizieren Sie mit Kollegen', imageUrl: 'assets/chat-app.png' },
+    { title: 'Pokemon Info', description: 'Informationen über Pokemon', imageUrl: 'assets/pokemon-info.png' }
+  ];
 }
